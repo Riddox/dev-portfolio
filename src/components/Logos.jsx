da9e0378dfd88d894ddb9bddb985f836ce9fe973
Riddox <thexiledoor@gmail.com>
2025-02-24 17:49:02 +0300

    added some logos and fixing buttons

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -9,7 +9,10 @@ const Logos = ({ className }) => {
       </h5>
       <ul className="flex">
         {companyLogos.map((logo, index) => (
-          <li>
+          <li
+            className="flex items-center justify-center flex-1 h-[8.5rem]"
+            key={index}
+          >
             <img src={logo} width={134} height={28} alt="logo" />
           </li>
         ))}
