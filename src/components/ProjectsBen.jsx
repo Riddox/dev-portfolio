5b97ebfd00c93b3e740a9b7e75d1a7c13d3a06ce
Riddox <thexiledoor@gmail.com>
2025-03-01 18:58:21 +0300

    header position fix

--- src/components/ProjectsBen.jsx
+++ src/components/ProjectsBen.jsx
@@ -12,7 +12,7 @@ const ProjectsBen = () => {
       <div className="container relative z-2">
         <Heading
           className="md:max-w-md lg:max-w-2xl"
-          title="Curiosity is the biggest inspiration."
+          title="Curiousity Is The Biggest Inspiration"
         />
         <div className="flex flex-wrap gap-10 mb-10">
           {benefits.map((item) => (
