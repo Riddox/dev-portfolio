6466c5c1e110c971756909ade092f105e80d39cd
Riddox <thexiledoor@gmail.com>
2025-03-05 20:05:41 +0300

    collab

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -7,6 +7,7 @@ const Collaboration = () => {
       <div className="container lg:flex">
         <div className="max-w-[25rem]">
           <h2 className="h2 mb-4 md:mb-8">A Little About Me</h2>
+          <p></p>
         </div>
       </div>
     </Section>
